064b78551491a413556a21d4368658030552efb0
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-08-31 20:23:20 -0500

    first commit
    Creada la estructura del proyecto

---
+++ src/main/java/com/storecode/ProyectoFinalAppsApplication.java
@@ -0,0 +1,13 @@
+package com.storecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storecode/ProyectoFinalAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

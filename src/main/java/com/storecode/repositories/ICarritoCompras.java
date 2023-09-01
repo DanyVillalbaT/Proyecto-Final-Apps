71de719812a323d93ea3731e3110d80a53ab6230
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-01 11:05:22 -0500

    Creada la interfaz de CarritoCompras

---
+++ src/main/java/com/storecode/repositories/ICarritoCompras.java
@@ -0,0 +1,9 @@
+package com.storecode.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.storecode.models.CarritoCompras;
+
+public interface ICarritoCompras extends JpaRepository<CarritoCompras, Long>{
+
+}

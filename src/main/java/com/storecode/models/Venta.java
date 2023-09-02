853a32378b8fda87b794db0707fa0e240744cf2d
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-01 22:25:26 -0500

    Creada la entidad Venta y su interfaz

---
+++ src/main/java/com/storecode/models/Venta.java
@@ -0,0 +1,74 @@
+package com.storecode.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Venta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String estado;
+	private String fecha;
+	private double valorTotal;
+	private int idUsuario; //Sería la fk de usuario
+	//private DetalleVenta detalleVenta;
+	
+	public Venta() {
+		super();
+	}
+	
+	public Venta(long id, String estado, String fecha, double valorTotal, int idUsuario) {
+		super();
+		this.id = id;
+		this.estado = estado;
+		this.fecha = fecha;
+		this.valorTotal = valorTotal;
+		this.idUsuario = idUsuario;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
+}

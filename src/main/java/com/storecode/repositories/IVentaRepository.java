2bd2886590fd8000465ae58611995d4cc690be54
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-11 18:44:40 -0500

    Cambiado el nombre de la interfaz en inglés

--- src/main/java/com/storecode/repositories/IVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.storecode.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.storecode.models.Venta;
-
-public interface IVentaRepository extends JpaRepository<Venta, Long>{
-
-}

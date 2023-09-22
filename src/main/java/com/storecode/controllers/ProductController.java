4b530f950ca69080c94aa34a29d6c1911e7830c9
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-22 17:03:09 -0500

    developing product details

--- src/main/java/com/storecode/controllers/ProductController.java
+++ src/main/java/com/storecode/controllers/ProductController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.storecode.models.Product;
 import com.storecode.services.ProductService;
 
 @Controller
@@ -20,5 +23,11 @@ public class ProductController {
 	        return "product/list-products";
 	    }
 
+	    @GetMapping("/productdetail/{idProduct}")
+	   	public String viewDetail(@PathVariable("idProducto") long idProduct, Model model) {
+	   		Product product = productService.getByiId(idProduct);
+	   		model.addAttribute("product", product);
+	   		return "product/detail-product";
+	   	}
 	   
 }

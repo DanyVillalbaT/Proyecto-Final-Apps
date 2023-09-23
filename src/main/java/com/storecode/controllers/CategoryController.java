b842d607f470f0881552c294d587037d9200bdd9
Juan Manuel <arenasrinconj@gmail.com>
2023-09-22 23:05:30 -0500

    se realizó todo lo relacionado a la categoria, sin embargo hay un error con el thyleaf. #18

--- src/main/java/com/storecode/controllers/CategoryController.java
+++ src/main/java/com/storecode/controllers/CategoryController.java
@@ -1,10 +1,24 @@
 package com.storecode.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.storecode.services.CategoryService;
 
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
 
+	 @Autowired
+	 private CategoryService categoryService;
+	 
+	@GetMapping("/list-categories")
+    public String listCategories(Model model) {
+    	model.addAttribute("listCategories", categoryService.getAll());
+        return "category/list-categories";
+    }
+	
 }
---
+++ src/main/java/com/storecode/services/CategoryService.java
@@ -0,0 +1,32 @@
+package com.storecode.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.storecode.models.Category;
+import com.storecode.repositories.ICategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private ICategoryRepository categoryRepository;
+
+	public List<Category> getAll() {
+		return categoryRepository.findAll();
+	}
+
+	public Category getByiId(Long id) {
+		System.out.println(categoryRepository.findById(id).orElse(null));
+		return categoryRepository.findById(id).orElse(null);
+	}
+
+	public Category save(Category categoria) {
+		return categoryRepository.save(categoria);
+	}
+
+	public void delete(Long id) {
+		categoryRepository.deleteById(id);
+	}
+}

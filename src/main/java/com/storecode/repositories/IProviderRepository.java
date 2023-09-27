a321a5efef0941213aaef194553fb192a126c489
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-26 20:14:33 -0500

    Added repository annotation

--- src/main/java/com/storecode/repositories/IProviderRepository.java
+++ src/main/java/com/storecode/repositories/IProviderRepository.java
@@ -3,10 +3,12 @@ package com.storecode.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.storecode.models.Product;
 import com.storecode.models.Provider;
 
+@Repository
 public interface IProviderRepository extends JpaRepository<Provider, Integer>{
 	
 	//List to find the products by the provider

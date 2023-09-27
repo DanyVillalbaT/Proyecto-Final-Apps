1ff4afe0cd3f5f5733fd3108fc0c86468df37e27
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-09-26 20:06:24 -0500

    Added list of products by provider
    Consulta que retorna una lista de productos de acuerdo al id del proveedor

--- src/main/java/com/storecode/repositories/IProviderRepository.java
+++ src/main/java/com/storecode/repositories/IProviderRepository.java
@@ -1,9 +1,15 @@
 package com.storecode.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.storecode.models.Product;
 import com.storecode.models.Provider;
 
 public interface IProviderRepository extends JpaRepository<Provider, Integer>{
+	
+	//List to find the products by the provider
+	List<Product> findByProvider(Provider provider);
 
 }
